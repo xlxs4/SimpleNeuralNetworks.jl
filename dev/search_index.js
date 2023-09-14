0a436e36d24768296c0fe7915d00f9dc92377184
Documenter.jl <documenter@juliadocs.github.io>
2023-09-14 12:08:53 +0000

    build based on 0bc53c3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SimpleNeuralNetworks","category":"page"},{"location":"#SimpleNeuralNetworks","page":"Home","title":"SimpleNeuralNetworks","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SimpleNeuralNetworks.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SimpleNeuralNetworks]","category":"page"}]
+}
